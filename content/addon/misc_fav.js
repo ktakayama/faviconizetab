39557cd6c0308a89139ba0bcc9a62de3d8d99729
Kyosuke Takayama <loiseau@gmail.com>
2011-06-03 01:44:26 +0900

    minor bug fix.

--- content/addon/misc_fav.js
+++ content/addon/misc_fav.js
@@ -20,22 +20,33 @@ faviconize.override = {
    adjustTabstrip: function() {
       var _o = gBrowser.mTabContainer.adjustTabstrip;
       gBrowser.mTabContainer.adjustTabstrip = function() {
-         var io = faviconize.IO.init();
-         if(!io.getBool('misc.nothide') || this.mCloseButtons != 1 || this.childNodes.length == 1)
+         if(this.mCloseButtons != 1 || this.childNodes.length == 1)
             return _o.apply(this, arguments);
 
-         var scrollWidth = this.mTabstrip.boxObject.width - this.mTabClipWidth;
-         var clipWidth = 0;
+         var io = faviconize.IO.init();
+         var width, clipWidth;
+         if(io.getBool('misc.nothide')) {
+            width = this.mTabstrip.boxObject.width - this.mTabClipWidth;
+            clipWidth = 0;
 
-         for(var tab, i = 0;tab = this.tabbrowser.visibleTabs[i];i++) {
-            if(tab.hasAttribute("faviconized")) {
-               clipWidth += 52;
-            } else {
-               clipWidth += tab.getBoundingClientRect().width;
+            for(var tab, i = 0;tab = this.tabbrowser.visibleTabs[i];i++) {
+               if(tab.hasAttribute("faviconized")) {
+                  clipWidth += 52;
+               } else {
+                  clipWidth += tab.getBoundingClientRect().width;
+               }
+            }
+         } else {
+            clipWidth = this.mTabClipWidth;
+            for(let tab, i = this.tabbrowser._numPinnedTabs;tab = this.tabbrowser.visibleTabs[i];i++) {
+               if(!tab.hasAttribute("faviconized")) {
+                  width = tab.getBoundingClientRect().width;
+                  break;
+               }
             }
          }
 
-         if(scrollWidth > clipWidth) this.setAttribute("closebuttons", "alltabs");
+         if(width > clipWidth) this.setAttribute("closebuttons", "alltabs");
          else this.setAttribute("closebuttons", "activetab");
 
          var tabstripClosebutton = document.getElementById("tabs-closebutton");
