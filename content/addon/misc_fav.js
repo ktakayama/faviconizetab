f84bce951bdd470656563c151c7a803167587af2
Kyosuke Takayama <loiseau@gmail.com>
2011-08-24 17:00:16 +0900

    fixed minor problem.

--- content/addon/misc_fav.js
+++ content/addon/misc_fav.js
@@ -16,43 +16,42 @@ faviconize.miscFav = {
    }
 }
 
-faviconize.override = {
-   adjustTabstrip: function() {
-      var _o = gBrowser.mTabContainer.adjustTabstrip;
-      gBrowser.mTabContainer.adjustTabstrip = function() {
-         if(this.mCloseButtons != 1 || this.childNodes.length == 1)
-            return _o.apply(this, arguments);
+faviconize.override.adjustTabstrip = function() {
+   faviconize.o_adjustTabstrip = gBrowser.mTabContainer.adjustTabstrip;
 
-         var io = faviconize.IO.init();
-         var width, clipWidth;
-         if(io.getBool('misc.nothide')) {
-            width = this.mTabstrip.boxObject.width - this.mTabClipWidth;
-            clipWidth = 0;
+   gBrowser.mTabContainer.adjustTabstrip = function() {
+      if(this.mCloseButtons != 1 || this.childNodes.length == 1)
+         return faviconize.o_adjustTabstrip.apply(this, arguments);
 
-            for(var tab, i = 0;tab = this.tabbrowser.visibleTabs[i];i++) {
-               if(tab.hasAttribute("faviconized")) {
-                  clipWidth += 52;
-               } else {
-                  clipWidth += tab.getBoundingClientRect().width;
-               }
-            }
-         } else {
-            clipWidth = this.mTabClipWidth;
-            for(let tab, i = this.tabbrowser._numPinnedTabs;tab = this.tabbrowser.visibleTabs[i];i++) {
-               if(!tab.hasAttribute("faviconized")) {
-                  width = tab.getBoundingClientRect().width;
-                  break;
-               }
+      var io = faviconize.IO.init();
+      var width, clipWidth;
+      if(io.getBool('misc.nothide')) {
+         width = this.mTabstrip.boxObject.width - this.mTabClipWidth;
+         clipWidth = 0;
+
+         for(var tab, i = 0;tab = this.tabbrowser.visibleTabs[i];i++) {
+            if(tab.hasAttribute("faviconized")) {
+               clipWidth += 52;
+            } else {
+               clipWidth += tab.getBoundingClientRect().width;
             }
          }
-
-         if(width > clipWidth) this.setAttribute("closebuttons", "alltabs");
-         else this.setAttribute("closebuttons", "activetab");
-
-         var tabstripClosebutton = document.getElementById("tabs-closebutton");
-         if(tabstripClosebutton && tabstripClosebutton.parentNode == this._container)
-            tabstripClosebutton.collapsed = this.mCloseButtons != 3;
+      } else {
+         clipWidth = this.mTabClipWidth;
+         for(let tab, i = this.tabbrowser._numPinnedTabs;tab = this.tabbrowser.visibleTabs[i];i++) {
+            if(!tab.hasAttribute("faviconized")) {
+               width = tab.getBoundingClientRect().width;
+               break;
+            }
+         }
       }
+
+      if(width > clipWidth) this.setAttribute("closebuttons", "alltabs");
+      else this.setAttribute("closebuttons", "activetab");
+
+      var tabstripClosebutton = document.getElementById("tabs-closebutton");
+      if(tabstripClosebutton && tabstripClosebutton.parentNode == this._container)
+         tabstripClosebutton.collapsed = this.mCloseButtons != 3;
    }
 }
 
